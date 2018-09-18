6d4c0105cf02b8f22f4429376a4740a78e057fb4
aa31 <5469434@qq.com>
2018-09-18 18:07:35 +0800

    '0918ui修改'

--- app.py
+++ app.py
@@ -59,6 +59,12 @@ def network():
     return render_template('network.html')
 
 
+class If():
+    @app.route('/get_sysstatus')
+    def get_sysstatus():
+        return 'sysstate'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=app.config['PORT'])
 
