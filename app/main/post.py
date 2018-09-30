19cc24365ddd9dfde8ac0e39f5dd6c46a161cd2e
aa31 <5469434@qq.com>
2018-09-30 11:30:57 +0800

    '0930'

--- app/main/post.py
+++ app/main/post.py
@@ -96,11 +96,10 @@ def reboot():
 
 @post_bp.route("/changenetwork/", methods=['GET'])
 def changenetwork():
-    arg1 = "192.168.80.101"
-    arg2 = "255.255.255.0"
-    arg3 = '192.168.1.1'
-    str = os.popen('sh ./app/sh/changenetwork.sh ' + arg1 + ' ' + arg2 + ' ' + arg3)
-    print(str)
+    ip = "192.168.80.101"
+    netmask = "255.255.255.0"
+    gw = '192.168.1.1'
+    os.system('sh ./app/sh/changenetwork.sh ' + ip + ' ' + netmask + ' ' + gw)
     return "{}"
 
 
